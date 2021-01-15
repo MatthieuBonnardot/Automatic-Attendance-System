3348e5a8ca6721d72af37da9681a99069e5c2887
Matthieu Cesar Bonnardot <matthieubonnardot88@gmail.com>
2021-01-15 12:53:19 +0100

    Machine Learning model Loading process
    - Added the ML script
    
    - Added State transition for loading the model.
    
    - Transition to webcam.

--- automatic-attendance-system/pages/index.js
+++ automatic-attendance-system/pages/index.js
@@ -2,6 +2,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+// // Import MachineLearning Model
+// import * as faceapi from "face-api.js";
+
 // Import Helper components
 import WebcamCapture from "../Components/Helper_Components/WebcamCapture";
 
@@ -9,7 +12,25 @@ import WebcamCapture from "../Components/Helper_Components/WebcamCapture";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [isLoading, setLoadingState] = useState(false);
+  const [isLoading, setLoadingState] = useState(true);
+
+  useEffect(() => {
+    const loadMachineLearningModel = () => {
+      Promise.all([
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+      ]).then(
+        console.log(
+          "The Machine Learning Model has been loaded successfully",
+          faceapi
+        )
+      );
+      if (faceapi) setLoadingState(false);
+    };
+
+    loadMachineLearningModel();
+  });
 
   return (
     <>
@@ -17,10 +38,17 @@ export default function Home() {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <script defer src="face-api.min.js"></script>
       </Head>
 
       <div className={styles.container}>
-        <WebcamCapture></WebcamCapture>
+        {isLoading ? (
+          <div>
+            <h1>Loading...</h1>
+          </div>
+        ) : (
+          <WebcamCapture />
+        )}
       </div>
     </>
   );
